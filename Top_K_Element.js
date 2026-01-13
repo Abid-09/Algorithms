0f6f1f5527b48dd07e11f8a7193043437ee66aaf
Abid-09 <hasanmollah151@gmail.com>
2026-01-13 19:50:12 +0100

    top k element
    Given an integer array nums and an integer k, return the k most frequent elements within the array.
    
    The test cases are generated such that the answer is always unique.
    
    You may return the output in any order.
    
    Example 1:
    
    Input: nums = [1,2,2,3,3,3], k = 2
    
    Output: [2,3]
    Example 2:
    
    Input: nums = [7,7], k = 1
    
    Output: [7]
    Constraints:
    
    1 <= nums.length <= 10^4.
    -1000 <= nums[i] <= 1000
    1 <= k <= number of distinct elements in nums.

---
+++ Top_K_Element.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const freq = new Map();
+    for (let n of nums) 
+    {
+        freq.set(n, (freq.get(n) || 0) + 1);
+    }
+    const heap = [...freq.entries()];
+    heap.sort((a, b) => b[1] - a[1]);
+
+    return heap.slice(0, k).map(e => e[0]);
+        }
+    }
